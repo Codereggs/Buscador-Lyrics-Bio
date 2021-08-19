95dcf7a3dba6369278a82cc34d39771c457c827f
MrGarsan <estebangabrielgarciasanchez@gmail.com>
2021-08-19 19:03:30 -0300

    feat wallpaper animation with svg

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Footer } from "./components/Footer";
 import { SongSearch } from "./components/SongSearch";
 import "./App.css";
 import Encabezado from "./components/Encabezado";
+import { FondoMusica } from "./components/FondoMusica";
 
 function App() {
   return (
     <div className="App">
       <Encabezado />
       <SongSearch />
+      <FondoMusica />
       <Footer />
     </div>
   );
---
+++ src/components/FondoMusica.js
@@ -0,0 +1,16 @@
+import React from "react";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+
+export const FondoMusica = () => {
+  return (
+    <>
+      <div className="musical-notes">
+        <MusicNoteIcon className="musical-note" />
+        <MusicNoteIcon className="musical-note" />
+        <MusicNoteIcon className="musical-note" />
+        <MusicNoteIcon className="musical-note" />
+        <MusicNoteIcon className="musical-note" />
+      </div>
+    </>
+  );
+};
